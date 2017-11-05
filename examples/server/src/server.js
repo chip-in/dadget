3faff3e577496697c3469d066ea2a52140834bfb
handa <handa@localhost.localdomain>
2017-11-05 18:07:01 +0900

    read /proc/1/environ

--- examples/server/src/server.js
+++ examples/server/src/server.js
@@ -5,9 +5,20 @@ import {
   , UpdateManager
   , QueryHandler
   , SubsetStorage} from '@chip-in/dadget';
+import fs from 'fs'
 
-const CORE_SERVER = process.env.CORE_SERVER ? process.env.CORE_SERVER : "http://test-core.chip-in.net";
-const RN_NAME = process.env.RN_NAME ? process.env.RN_NAME : "db-server";
+let env = {}
+try {
+  let text = fs.readFileSync('/proc/1/environ', { encoding: 'utf-8' });
+  let envList = text.split("\0");
+  envList.forEach(row => {
+    let data = row.split("=", 2);
+    env[data[0]] = data[1];
+  })
+} catch (e) {}
+
+const CORE_SERVER = process.env.CORE_SERVER ? process.env.CORE_SERVER : env.CORE_SERVER ? env.CORE_SERVER : "http://test-core.chip-in.net";
+const RN_NAME = process.env.RN_NAME ? process.env.RN_NAME : env.RN_NAME ? env.RN_NAME : "db-server";
 
 let node = new ResourceNode(CORE_SERVER, RN_NAME);
 node.registerServiceClasses({
