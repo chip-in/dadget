1015a9533a5b1fb80ee45d80029a3f5e2581c889
handa <31386906+handa9@users.noreply.github.com>
2019-03-20 23:02:53 +0900

    Fix the process on connection error

--- src/se/SubsetStorage.ts
+++ src/se/SubsetStorage.ts
@@ -24,7 +24,6 @@ import { UpdateManager } from "./UpdateManager";
 class UpdateProcessor extends Subscriber {
 
   private lock: ReadWriteLock;
-  private masterManagerUuid?: string;
 
   constructor(
     protected storage: SubsetStorage,
@@ -44,13 +43,6 @@ class UpdateProcessor extends Subscriber {
 
   procTransaction(transaction: TransactionObject) {
     this.logger.info("procTransaction:", transaction.type, transaction.csn);
-    if (transaction.type === TransactionType.ROLLBACK) {
-      this.masterManagerUuid = transaction.manager;
-    }
-    if (this.masterManagerUuid && this.masterManagerUuid !== transaction.manager) {
-      this.logger.warn("ignored transaction:", transaction.csn);
-      return;
-    }
 
     if (transaction.protectedCsn) {
       if (this.storage.getJournalDb().getProtectedCsn() < transaction.protectedCsn) {
